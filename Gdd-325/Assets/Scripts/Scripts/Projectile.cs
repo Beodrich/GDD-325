09e5f4fa737b433a39639c2db740186e49e0cdac
Cole <49996960+CWoulf@users.noreply.github.com>
2021-02-19 09:27:34 -0600

    Stuff

--- Gdd-325/Assets/Scripts/Scripts/Projectile.cs
+++ Gdd-325/Assets/Scripts/Scripts/Projectile.cs
@@ -7,15 +7,11 @@ public class Projectile : MonoBehaviour
     public float speed;
     public float lifeTime;
     public float distance;
-    //public List<Golem> golemList;
-    public Golem golem;
     public LayerMask whatIsSolid;
-
-    //public GameObject destroySpell;
 
     private void Start()
     {
-        //Invoke("DestroyProjectile", lifeTime);
+        Invoke("DestroyProjectile", lifeTime);
     }
     private void Update()
     {
@@ -25,16 +21,50 @@ public class Projectile : MonoBehaviour
             if (hit.collider.CompareTag("enemy"))
             {
                 Debug.Log("Enemy Take Damage");
-                this.golem.TakeDamage();
             }
             DestroyProjectile();
         }
+
         transform.Translate(transform.up * speed * Time.deltaTime);
     }
-
     private void DestroyProjectile()
     {
         //Instantiate(destroySpell,transform.position,Quaternion.identity);
         Destroy(gameObject);
     }
-}
+
+        /*    old projectile class
+         *    public float speed;
+            public float lifeTime;
+            public float distance;
+            //public List<Golem> golemList;
+            public Golem golem;
+            public LayerMask whatIsSolid;
+
+            //public GameObject destroySpell;
+
+            private void Start()
+            {
+                //Invoke("DestroyProjectile", lifeTime);
+            }
+            private void Update()
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+                if (hit.collider != null)
+                {
+                    if (hit.collider.CompareTag("enemy"))
+                    {
+                        Debug.Log("Enemy Take Damage");
+                        this.golem.TakeDamage();
+                    }
+                    DestroyProjectile();
+                }
+                transform.Translate(transform.up * speed * Time.deltaTime);
+            }
+
+            private void DestroyProjectile()
+            {
+                //Instantiate(destroySpell,transform.position,Quaternion.identity);
+                Destroy(gameObject);
+            }*/
+    }
--- Gdd-325/Assets/wand.cs
+++ Gdd-325/Assets/wand.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class wand : MonoBehaviour
 {
     public float offset;
+
+    public GameObject projectile;
+    public Transform shotPoint;
+
+    private float timeBTWShots;
+    public float startTimeBTWShots = 0.25f;
+
     private void Update()
     {
+        // Handles the weapon rotation
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ+ offset);
+
+        if (timeBTWShots <=0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBTWShots = startTimeBTWShots;
+            }
+        }
+        else
+        {
+            timeBTWShots -= Time.deltaTime;
+        }
     }
+
 }
