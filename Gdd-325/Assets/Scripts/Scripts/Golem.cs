d965016811294ff5e535fb2c5669e613f5de5067
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-02-24 09:52:11 -0600

    Update Golem.cs

--- Gdd-325/Assets/Scripts/Scripts/Golem.cs
+++ Gdd-325/Assets/Scripts/Scripts/Golem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Spells;
 
 public class Golem : MonoBehaviour
 {
@@ -25,38 +26,39 @@ public class Golem : MonoBehaviour
             //Instantiate(deathEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
+        GetComponent<AIPath>
     }
 
-    public void TakeDamage()
+    public void TakeDamage(SpellState spell)
     {
-        //change the state 
-        if (spells.fire == true)
-        {
-           
-            health -= 5;
-        }
-        /*
-        if (spells.ice == true)
-        {
-            health -= 5;
-        }
-        if (spells.earth == true)
-        {
-            health -= 5;
-        }
-        if (spells.air == true)
-        {
-            health -= 5;
-        }
-        if (spells.base == true)
-        {
-            health -= 5;
-        }*/
+        switch (spell) {
+            case SpellState.Fire:
+                //code here
+                health -= 5;
+                break;
+            case SpellState.Ice:
+                //code here
+                break;
+            case SpellState.Earth:
+                //code
+                break;
+            case SpellState.Air:
+                //code
+                break;
+            default://none case
+                    //code here
+                break;
 
-        if (health <= 0)
-        {
-            Destroy(gameObject);
+
+
+
+
+
         }
+
+
+
+
     }
 
     public void PlayerDamaged()
