bd5f7485e04f39a95b9d50ad41427d494476b304
Cole <49996960+CWoulf@users.noreply.github.com>
2021-02-26 12:17:07 -0600

    ummm

---
+++ Gdd-325/Assets/Scripts/Mouse Aim/testing.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour
+{
+    [SerializeField] private PlayerAimWeapon playerAimWeapon;
+
+    private void Start()
+    {
+        //playerAimWeapon.OnShoot += PlayerAimWeapon_Shoot;
+    }
+}
