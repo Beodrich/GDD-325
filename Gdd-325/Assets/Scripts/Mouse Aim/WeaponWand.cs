40494de8e16eca6778ad8f85bae71270fc7a8233
Cole <49996960+CWoulf@users.noreply.github.com>
2021-03-12 08:33:35 -0600

    Volume Mixer added and incorporated into main and pause menu

--- Gdd-325/Assets/Scripts/Mouse Aim/WeaponWand.cs
+++ Gdd-325/Assets/Scripts/Mouse Aim/WeaponWand.cs
@@ -15,10 +15,13 @@ public class WeaponWand : MonoBehaviour
     void Update()
     {
         //Debug.Log(isShoot);
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-
+        if (!PauseMenu.GameIsPaused)
+        {
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+        }
+       
         if(timeBTWShots <= 0)
         {
             if (Input.GetMouseButtonDown(0) && Mana>0.5)
