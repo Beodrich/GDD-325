5409fd746249a1aa9f2ad94535da7515d635e1a1
Cole <49996960+CWoulf@users.noreply.github.com>
2021-03-22 12:10:53 -0500

    Wand UI Stuff

--- Gdd-325/Assets/Scripts/Scripts/PickUpWand.cs
+++ Gdd-325/Assets/Scripts/Scripts/PickUpWand.cs
@@ -13,6 +13,13 @@ public class PickUpWand : MonoBehaviour
     public GameObject Earthwand;
     public GameObject Earth;
 
+    // UI
+    public GameObject FireUI; 
+    public GameObject IceUI;
+    public GameObject WindUI;
+    public GameObject EarthUI;
+
+
     private PlayerController player;
 
     private void Start()
@@ -20,12 +27,19 @@ public class PickUpWand : MonoBehaviour
         player = GameObject.Find("MonkE").GetComponent<PlayerController>();
         Fire.SetActive(true);
         Ice.SetActive(true);
+        Earth.SetActive(true);
+        Wind.SetActive(true);
+        FireUI.SetActive(false);
+        IceUI.SetActive(false);
+        WindUI.SetActive(false);
+        EarthUI.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject == Fire)
         {
+            // Sets GameObjects
             Firewand.SetActive(true);
             Fire.SetActive(false);
             Ice.SetActive(true);
@@ -34,13 +48,20 @@ public class PickUpWand : MonoBehaviour
             Wind.SetActive(true);
             Earthwand.SetActive(false);
             Earth.SetActive(true);
+            // Sets Player Bools
             player.setFire(false);
             player.setIce(true);
             player.setWind(false);
             player.setEarth(false);
+            // Sets UI Wands
+            FireUI.SetActive(true);
+            IceUI.SetActive(false);
+            WindUI.SetActive(false);
+            EarthUI.SetActive(false);
         }
         if (col.gameObject == Ice)
         {
+            // Sets GameObjects
             Ice.SetActive(false);
             Icewand.SetActive(true);
             Fire.SetActive(true);
@@ -49,13 +70,20 @@ public class PickUpWand : MonoBehaviour
             Wind.SetActive(true);
             Earthwand.SetActive(false);
             Earth.SetActive(true);
+            // Sets Player Bools
             player.setFire(false);
             player.setIce(true);
             player.setWind(false);
             player.setEarth(false);
+            // Sets UI Wands
+            FireUI.SetActive(false);
+            IceUI.SetActive(true);
+            WindUI.SetActive(false);
+            EarthUI.SetActive(false);
         }
-        if(col.gameObject == Wind)
+        if (col.gameObject == Wind)
         {
+            // Sets GameObjects
             Ice.SetActive(true);
             Icewand.SetActive(false);
             Fire.SetActive(true);
@@ -64,13 +92,20 @@ public class PickUpWand : MonoBehaviour
             Wind.SetActive(false);
             Earthwand.SetActive(false);
             Earth.SetActive(true);
+            // Sets Player Bools
             player.setFire(false);
             player.setIce(false);
             player.setWind(true);
             player.setEarth(false);
+            // Sets UI Wands
+            FireUI.SetActive(false);
+            IceUI.SetActive(false);
+            WindUI.SetActive(true);
+            EarthUI.SetActive(false);
         }
-        if(col.gameObject == Earth)
+        if (col.gameObject == Earth)
         {
+            // Sets GameObjects
             Ice.SetActive(true);
             Icewand.SetActive(false);
             Fire.SetActive(true);
@@ -79,10 +114,16 @@ public class PickUpWand : MonoBehaviour
             Wind.SetActive(true);
             Earthwand.SetActive(true);
             Earth.SetActive(false);
+            // Sets Player Bools
             player.setFire(false);
             player.setIce(false);
             player.setWind(false);
             player.setEarth(true);
+            // Sets UI Wands
+            FireUI.SetActive(false);
+            IceUI.SetActive(false);
+            WindUI.SetActive(false);
+            EarthUI.SetActive(true);
         }
     }
 }
