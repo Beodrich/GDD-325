2abbf3442865d372edbbfee5444e56e49edec032
Cole <49996960+CWoulf@users.noreply.github.com>
2021-04-02 09:35:39 -0500

    Update WeaponWand.cs

--- Gdd-325/Assets/Scripts/Mouse Aim/WeaponWand.cs
+++ Gdd-325/Assets/Scripts/Mouse Aim/WeaponWand.cs
@@ -25,12 +25,14 @@ public class WeaponWand : MonoBehaviour
         //Debug.Log(isShoot);
         if (!PauseMenu.GameIsPaused)
         {
-             difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+            shootSpells();
         }
             
-        shootSpells();
+       
         
     }
 
