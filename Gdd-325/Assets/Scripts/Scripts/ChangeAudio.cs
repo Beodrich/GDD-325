05ed94a1807d1e5e29ab5c9c7add152cbdfc61ea
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-04-23 08:33:35 -0500

    worked changing boss

---
+++ Gdd-325/Assets/Scripts/Scripts/ChangeAudio.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource backRoundMusic;
+    [SerializeField] private AudioSource bossMusic;
+    private AudioSource currentAudio;
+    [SerializeField] private GameObject boss;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentAudio = backRoundMusic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boss.activeSelf)
+        {
+            ChangeAudioSource(bossMusic);
+
+        }
+        else {
+            ChangeAudioSource(backRoundMusic);
+
+        }
+    }
+    void ChangeAudioSource(AudioSource audio) {
+        if (currentAudio == audio) {
+            return;
+
+        }
+        currentAudio.Stop();
+        currentAudio = audio;
+        currentAudio.Play();
+
+    }
+}
+
+
+ 
